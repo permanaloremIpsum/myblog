6f031e0e60990749663d4db4ad71613334e847ab
permanaloremIpsum <dpermana350@gmail.com>
2017-08-11 08:57:05 +0700

    update

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^zzq6mf77ryn*i)xwaag%4hw90ra)s@^%)m%ms&xhendoe^5k7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'article',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
