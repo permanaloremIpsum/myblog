64fb2c920fa516ceebe114c9b0d7d6642b84f785
permanaloremIpsum <dpermana350@gmail.com>
2017-08-23 10:41:53 +0700

    django view dan template

--- article/views.py
+++ article/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from article.models import Category, Post
 
 # Create your views here.
 def myview(request):
@@ -13,4 +14,37 @@ def nama(request, nama_saya, umur):
 	return HttpResponse('Nama saya %s, umur saya adalah %s tahun' % (nama_saya, umur))
 
 def home(request):
-	return render(request, 'index.html')
+	categories = Category.objects.all()
+	posting = Post.objects.all().order_by('-created_on')[:3]
+	data = {
+		'categories': categories,
+		'posting' : posting,
+	}
+	return render(request, 'index.html', data)
+
+def single(request, slug):
+	categories = Category.objects.all()
+	posting = Post.objects.get(slug=slug)
+	data = {
+		'categories': categories,
+		'posting' : posting,
+	}
+	return render(request, 'single.html', data)
+
+def cat(request):
+	categories = Category.objects.all()
+	data = {
+		'categories': categories,
+	}
+	return render(request, 'category.html', data)
+
+def list_cat(request, id):
+	catlist = Category.objects.get(id=id)
+	categories = Category.objects.all()
+	post = Post.objects.filter(category=catlist)
+	data = {
+		'catlist': catlist,
+		'categories': categories,
+		'post': post,
+	}
+	return render(request, 'list_cat.html', data)
